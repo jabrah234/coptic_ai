0f9470a1c38aa193b50781d5dbbb4d02dea49da1
jabrah234 <103787049+jabrah234@users.noreply.github.com>
2025-05-12 18:28:35 -0400

    Delete venv directory

--- venv/bin/activate.csh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13eb9eed37ab6540c633baa14bb5ea263df468b0a2952c55c351ec30b0bd862a
-size 864
--- venv/bin/activate.fish
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:68fe648e049af43d74a7ff430278302f3df1f27ff4e698cb73ca1e9ea5bed0d1
-size 2004
